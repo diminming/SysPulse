94d9e0eda05afba4a8fc8ac9727087aae0b95ce0
Matt <diminming@outlook.com>
2024-09-03 21:16:12 +0800

    调整拓扑视图外观设计

--- facade/src/layout/Topological.vue
+++ facade/src/layout/Topological.vue
@@ -1,7 +1,17 @@
 <template>
+    <div style="text-align: right; margin-top: 1rem;">
+        <a-range-picker v-model:value="value2" show-time />
+    </div>
+    <div style="position: absolute;bottom: 5rem;right: 5rem;">
+        <a-button shape="circle" :icon="h(CopyOutlined)" />
+    </div>
+    <div>
+        <a-slider v-model:value="value2" range :disabled="false" />
+    </div>
     <div class="graph full_height">
         <canvas id="topo" width="1280" height="960"></canvas>
     </div>
+    
 </template>
 
 <script setup lang="ts">
@@ -9,11 +19,13 @@ import * as echarts from 'echarts';
 import { onMounted } from 'vue';
 import request from '@/utils/request';
 import data from "@/layout/demo"
+import { h } from 'vue';
+import { SearchOutlined, CopyOutlined } from '@ant-design/icons-vue';
 
 onMounted(() => {
     let container: any = document.querySelector(".graph")
     let chart_dom: any = document.getElementById('topo')
-    
+
     let chart = echarts.init(chart_dom, undefined, {
         width: container.offsetWidth,
         height: container.offsetHeight
@@ -22,68 +34,48 @@ onMounted(() => {
 
     chart.showLoading();
 
-    let json = data
+    let webkitDep = data
     chart.hideLoading();
     chart.setOption(
         (option = {
-            title: {
-                text: 'NPM Dependencies'
+            legend: {
+                data: ['Linux', 'Database', 'Middleware', 'Cache', 'Other']
             },
-            animationDurationUpdate: 1500,
-            // animationEasingUpdate: 'quinticInOut',
             series: [
                 {
                     type: 'graph',
-                    layout: 'none',
-                    // progressiveThreshold: 700,
-                    data: json.nodes.map(function (node) {
-                        return {
-                            x: node.x,
-                            y: node.y,
-                            id: node.id,
-                            name: node.label,
-                            symbolSize: node.size,
-                            itemStyle: {
-                                color: node.color
-                            }
-                        };
-                    }),
-                    edges: json.edges.map(function (edge) {
-                        return {
-                            source: edge.sourceID,
-                            target: edge.targetID
-                        };
-                    }),
-                    emphasis: {
-                        focus: 'adjacency',
-                        label: {
-                            position: 'right',
-                            show: true
-                        }
+                    layout: 'force',
+                    animation: false,
+                    label: {
+                        position: 'right',
+                        formatter: '{b}'
                     },
-                    roam: true,
-                    lineStyle: {
-                        width: 0.5,
-                        curveness: 0.3,
-                        opacity: 0.7
-                    }
+                    draggable: true,
+                    data: webkitDep.nodes.map(function (node, idx) {
+                        node.id = idx;
+                        return node;
+                    }),
+                    categories: webkitDep.categories,
+                    force: {
+                        edgeLength: 5,
+                        repulsion: 20,
+                        gravity: 0.2
+                    },
+                    edges: webkitDep.links
                 }
             ]
         }),
-        true
+    true
     );
 
 
-    option && chart.setOption(option);
+option && chart.setOption(option);
 })
 
 </script>
 <style scoped>
-
 .graph {
     width: 100%;
     text-align: center;
 }
-
-
 </style>
