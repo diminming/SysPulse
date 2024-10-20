04ac144410c508242d365ecf458b82b130bdab65
diminming <diminming@outlook.com>
2024-10-20 16:40:31 +0800

    完成linux tracker的构建脚本

--- tracker/linux/build_4_staging.sh
+++ tracker/linux/build_4_staging.sh
@@ -9,8 +9,15 @@ if [ ! -d $OUTPUT_DIR ];then
     mkdir -p $OUTPUT_DIR
 fi
 
-CC=musl-gcc go build -ldflags '-extldflags "-static -lpcap -ldbus-1 -lsystemd"' -o $OUTPUT_DIR/$BIN_NAME
+# go clean -modcache
+
+# export LD_LIBRARY_PATH="-L/workspace/tmp/libpcap-1.10.5"
+# export CGO_LDFLAGS="-L/workspace/tmp/libpcap-1.10.5 -extldflags '-static'"
+# export CGO_CPPFLAGS="-I/workspace/tmp/libpcap-1.10.5"
+
+# go build -v -o $OUTPUT_DIR/$BIN_NAME
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build --ldflags "-extldflags \"-static\"" -a -o $OUTPUT_DIR/$BIN_NAME .
+
+# go build -ldflags='-s -w -L /workspace/tmp/libpcap-1.10.5 -extldflags "-static"'
 
 cp config_staging.yaml $OUTPUT_DIR/config.yaml
-
-
