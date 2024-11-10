69dc5f8c558fed7723654692fea53849f3463c22
diminming <diminming@outlook.com>
2024-11-10 09:16:08 +0800

    修正filerserver镜像构建逻辑

--- docker/syspulse_fileserver/build_img.sh
+++ docker/syspulse_fileserver/build_img.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 
-# 由于minio基础镜像通过环境变量进行设置即可满足需求，
-# 因此未制作镜像，该脚本仅是导出minio的最新镜像
-
 DOCKER_IMAGE_NAME="syspulse_fileserver"
 
-docker save -o ${DOCKER_IMAGE_NAME}.tar minio/minio
+docker build -t ${DOCKER_IMAGE_NAME} .
+docker save -o ${DOCKER_IMAGE_NAME}.tar ${DOCKER_IMAGE_NAME}
