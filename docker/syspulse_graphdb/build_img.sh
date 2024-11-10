103c74ebf5023b437ee5bbaa0d01aa6405c2e718
diminming <diminming@outlook.com>
2024-11-10 09:17:40 +0800

    修正graphdb镜像的构建逻辑

--- docker/syspulse_graphdb/build_img.sh
+++ docker/syspulse_graphdb/build_img.sh
@@ -2,22 +2,5 @@
 
 DOCKER_IMAGE_NAME="syspulse_graphdb"
 
-ENDPOINT="tcp://localhost:8529"
-USER="root"
-DATABASE="insight"
-OUTPUT="/workspace/SysPulse/docker/syspulse_graphdb/syspulse"
-
-if [ ! -d "${OUTPUT}" ]; then
-  mkdir -p ${OUTPUT}
-fi
-
-arangodump \
-  --server.endpoint ${ENDPOINT} \
-  --include-system-collections true \
-  --server.username ${USER} \
-  --server.database ${DATABASE} \
-  --overwrite true \
-  --output-directory ${OUTPUT}
-
 docker build -t ${DOCKER_IMAGE_NAME} .
 docker save -o ${DOCKER_IMAGE_NAME}.tar ${DOCKER_IMAGE_NAME}
