e97e50fc1402cb491ec2a2aa85c3ab848ef301a6
diminming <diminming@outlook.com>
2024-11-10 09:17:00 +0800

    修正sqldb镜像的构建逻辑

--- docker/syspulse_sqldb/build_img.sh
+++ docker/syspulse_sqldb/build_img.sh
@@ -10,9 +10,9 @@ cat > 01-create_db.sql << 'EOF'
 CREATE DATABASE `syspulse` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 EOF
 
-mysqldump -h${DB_HOST} -u${DB_USER} -p${DB_PASSED} --opt -d ${DB_NAME} > 02-syspulse.sql
+# mysqldump -h${DB_HOST} -u${DB_USER} -p${DB_PASSED} --opt -d ${DB_NAME} > 02-syspulse.sql
 
-sed -i "1i USE \`syspulse\`" 02-syspulse.sql
+# sed -i "1i USE \`syspulse\`" 02-syspulse.sql
 
 cat > 03-create_user.sql << 'EOF'
 create user syspulse identified by '123qweASD';
